a6468005f40e93666a964ad0c3d8be09133d685e
wendannor <jordan.sarre@gmail.com>
2015-06-06 19:11:53 +0200

    add ttl to

--- src/scope.js
+++ src/scope.js
@@ -13,22 +13,41 @@ Scope.prototype.$watch = function (watchFn, listenerFn) {
 
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn || function () {},
+        listenerFn: listenerFn || function () {
+        },
         lastValue: initWatchVal
     };
     this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$$digestOnce = function () {
     var _this = this;
     var oldValue;
     var newValue;
+    var dirty = false;
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(_this);
         oldValue = watcher.lastValue;
         if (newValue !== oldValue) {
             watcher.lastValue = newValue;
             watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), _this);
+            dirty = true;
         }
     });
+    return dirty;
+};
+
+Scope.prototype.$digest = function () {
+    var dirty;
+    
+    // time to live, number of cycles we allow the digest loop to make
+    var ttl = 10;
+    do {
+        dirty = this.$$digestOnce();
+        ttl--;
+    } while (dirty && ttl !== 0);
+    
+    if (ttl === 0) {
+        throw '10 digest iterations reached';
+    }
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -104,6 +104,51 @@ describe('Scope', function () {
         });
 
 
+        it('triggers chained watchers in the same digest', function () {
+            scope.name = 'John';
+            
+            scope.$watch(function (scope) {
+                return scope.nameUpper;
+            }, function (newValue, oldValue, scope) {
+                if (newValue) {
+                    scope.initial = newValue.substring(0, 1) + '.';
+                }
+            });
+            
+            scope.$watch(function (scope) {
+                return scope.name;
+            }, function (newValue, oldValue, scope) {
+                if (newValue) {
+                    scope.nameUpper = newValue.toUpperCase();
+                }
+            });
+            
+            scope.$digest();
+            expect(scope.initial).toBe('J.');
+            scope.name = 'Bob';
+            scope.$digest();
+            expect(scope.initial).toBe('B.');
+        });
+
+        it('gives up on the watches after 10 iterations', function () {
+            scope.counterA = 0;
+            scope.counterB = 0;
+            
+            scope.$watch(function (scope) {
+                return scope.counterA;
+            }, function (newValue, oldValue, scope) {
+                scope.counterB++;
+            });
+            scope.$watch(function (scope) {
+                return scope.counterB;
+            }, function (newValue, oldValue, scope) {
+                scope.counterA++;
+            });
+            
+            expect((function(){scope.$digest()})).toThrow();
+        });
+
+
     });
 
 
